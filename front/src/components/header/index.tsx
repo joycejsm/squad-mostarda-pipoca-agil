82522d67c3a09ece3c5331dc778c55bd6de280d6
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2025-09-21 13:43:06 -0300

    estilizando o header

--- front/src/components/header/index.tsx
+++ front/src/components/header/index.tsx
@@ -1,9 +1,10 @@
 import styles from './styles.module.scss';
+import Link from 'next/link'
 
-export default function Header(){
-  return(
-    <header>
-      header works!
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <Link className={styles.logo} href="/">GoMoney</Link>
     </header>
   )
 }
